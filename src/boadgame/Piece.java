42099ebce930949a3e1e34b18c9888c04cebe057
Igor Rocha <igordalcerorocha@gmail.com>
2021-01-15 18:55:52 -0300

    Starting to implement Board and Piece

---
+++ src/boadgame/Piece.java
@@ -0,0 +1,28 @@
+package boadgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+	}
+	
+	
+	public Piece(Position position, Board board) {
+		this.board = board;
+		position = null;
+	}
+
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+	
+	
+	
+	
+
+}
