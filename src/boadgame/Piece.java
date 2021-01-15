18b68839e1e7d4a01d289a06d4ac9fb7b81f162c
Igor Rocha <igordalcerorocha@gmail.com>
2021-01-15 19:40:18 -0300

    Chess layer and printing the board

--- src/boadgame/Piece.java
+++ src/boadgame/Piece.java
@@ -1,28 +1,17 @@
 package boadgame;
 
 public class Piece {
-	
+
 	protected Position position;
 	private Board board;
-	
-	public Piece() {
-	}
-	
-	
-	public Piece(Position position, Board board) {
+
+	public Piece(Board board) {
 		this.board = board;
 		position = null;
 	}
 
-
 	protected Board getBoard() {
 		return board;
 	}
-	
-	
-	
-	
-	
-	
 
 }
